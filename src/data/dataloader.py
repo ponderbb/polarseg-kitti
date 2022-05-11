5e268bfc425083f37a40b6bb3049985dbeb98d0f
bbejczy <bbejczy@icloud.com>
2022-05-11 14:00:03 +0000

    semanticKitti class

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,51 +1,66 @@
 import os
 from pathlib import Path
 
+import numpy as np
+import torch
 import yaml
 from torch.utils.data import Dataset
 
 
 class SemanticKITTI(Dataset):
     def __init__(self, path, data_split="train") -> None:
-        self.data_split = data_split
-        self.image_list = []
 
         with open("semantic-kitti.yaml", "r") as stream:
-            dataset_config = yaml.safe_load(stream)
+            dataset_yaml = yaml.safe_load(stream)
 
+        self.data_split = data_split
+        self.learning_map = dataset_yaml["learning_map"]
+        self.scan_list = []
+        self.label_list = []
         try:
-            split = dataset_config["split"][self.data_split]
+            split = dataset_yaml["split"][self.data_split]
             print(split)
         except ValueError:
             print("Incorrect set type")
 
         for sequence_folder in split:
-            self.image_list += getPath(
-                "/".join([path, "sequences", str(sequence_folder).zfill(2), "velodyne"])
-            )
-        self.image_list.sort()
-        print(len(self.image_list))
+            self.scan_list += getPath("/".join([path, "sequences", str(sequence_folder).zfill(2), "velodyne"]))
+            self.label_list += getPath("/".join([path, "sequences", str(sequence_folder).zfill(2), "labels"]))
+        self.scan_list.sort()
+        self.label_list.sort()
 
     def __len__(self):
-        return len(self.image_list)
+        return len(self.scan_list)
 
     def __getitem__(self, index):
+        scan = np.fromfile(self.scan_list[index], dtype=np.float32).reshape(-1, 4)
+        if self.data_split == "test":
+            labels = np.zeros(shape=scan[:, 0].shape, dtype=int)
+        else:
+            labels = np.fromfile(self.label_list[index], dtype=np.int32)
+            labels = labels & 0xFFFF  # according to the semanticKITTI apilab
+            labels[list(self.learning_map.keys())] = list(self.learning_map.values())  # remap from cross-entropy labels
+            labels = labels.reshape(-1, 1)
 
-        return None
+        data_tuple = (scan, labels)
+
+        return data_tuple
 
 
 def getPath(dir):
-    for root, dirs, files in os.walk(dir):
+    for root, _, files in os.walk(dir):
         for f in files:
             yield str(Path(os.path.join(root, f)))
 
 
 def main():
 
-    train_dataset = SemanticKITTI(
-        path="/root/repos/polarseg-kitti/data/debug", data_split="train"
-    )
-    print(train_dataset)
+    train_dataset = SemanticKITTI(path="/root/repos/polarseg-kitti/data/debug", data_split="test")
+    dummy_dataloader = torch.utils.data.DataLoader(train_dataset)
+
+    for data_tuple in dummy_dataloader:
+        print(data_tuple[0])
+        print(data_tuple[1])
 
 
 if __name__ == "__main__":
