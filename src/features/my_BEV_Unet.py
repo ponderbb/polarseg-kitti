350265e6048e70893452e7c20a87832bfd307d55
zoomin-lee <65997635+zoomin-lee@users.noreply.github.com>
2022-05-27 21:03:30 +0900

    change util function

--- src/features/my_BEV_Unet.py
+++ src/features/my_BEV_Unet.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-from src.features.utils import double_CBR, down_CBR, up_CBR
+from src.features.utils import down_CBR, up_CBR, CBR, BlockDrop
 
 
 class BEV_Unet(nn.Module):
@@ -23,27 +23,56 @@ class UNet(nn.Module):
     def __init__(self, n_class, n_height, dropout, circular_padding, block_size):
         super(UNet, self).__init__()
         self.norm = nn.BatchNorm2d(n_height)
-        self.inc = double_CBR(n_height, 64, circular_padding, 3)
-        self.down1 = down_CBR(64, 128, circular_padding, 3)
-        self.down2 = down_CBR(128, 256, circular_padding, 3)
-        self.down3 = down_CBR(256, 512, circular_padding, 3)
-        self.down4 = down_CBR(512, 512, circular_padding, 3)
-        self.up1 = up_CBR(1024, 256, circular_padding, 3, drop_p=dropout, block_size=block_size)
-        self.up2 = up_CBR(512, 128, circular_padding, 3, drop_p=dropout, block_size=block_size)
-        self.up3 = up_CBR(256, 64, circular_padding, 3, drop_p=dropout, block_size=block_size)
-        self.up4 = up_CBR(128, 64, circular_padding, 3, drop_p=dropout, block_size=block_size)
+        self.inc1 = CBR(n_height, 64, circular_padding, kernel_size = 3, padding_size=1)
+        self.conv0 = CBR(64, 64, circular_padding, kernel_size = 3, padding_size=1)
+        self.down1 = down_CBR(64, 128, circular_padding, kernel_size = 3, padding_size=1)
+        self.conv1 = CBR(128, 128, circular_padding, kernel_size=3, padding_size=1)
+        self.down2 = down_CBR(128, 256, circular_padding, kernel_size = 3, padding_size=1)
+        self.conv2 = CBR(256, 256, circular_padding, kernel_size=3, padding_size=1)
+        self.down3 = down_CBR(256, 512, circular_padding, kernel_size = 3, padding_size=1)
+        self.conv3 = CBR(512, 512, circular_padding, kernel_size=3, padding_size=1)
+        self.down4 = down_CBR(512, 512, circular_padding, kernel_size = 3, padding_size=1)
+        self.conv4 = CBR(512, 512, circular_padding, kernel_size=3, padding_size=1)
+        self.up1 = up_CBR(1024, 256, circular_padding, kernel_size=3, padding_size=1)
+        self.up2 = up_CBR(512, 128, circular_padding, kernel_size=3, padding_size=1)
+        self.up3 = up_CBR(256, 64, circular_padding, kernel_size=3, padding_size=1)
+        self.up4 = up_CBR(128, 64, circular_padding, kernel_size=3, padding_size=1)
+        self.drop = BlockDrop(drop_p=dropout, block_size=block_size)
         self.outc = nn.Conv2d(64, n_class, 1)
+
 
     def forward(self, x, circular_padding):
         x = self.norm(x)
-        x1 = self.inc(x, circular_padding)
+        x1 = self.inc1(x, circular_padding)
+        x1 = self.conv0(x1, circular_padding)
+
         x2 = self.down1(x1, circular_padding)
+        x2 = self.conv1(x2, circular_padding)
+
         x3 = self.down2(x2, circular_padding)
+        x3 = self.conv2(x3, circular_padding)
+
         x4 = self.down3(x3, circular_padding)
+        x4 = self.conv3(x4, circular_padding)
+
         x5 = self.down4(x4, circular_padding)
+        x5 = self.conv4(x5, circular_padding)
+
         x = self.up1(x5, x4, circular_padding)
+        x = self.conv2(x, circular_padding)
+        x = self.drop(x)
+
         x = self.up2(x, x3, circular_padding)
+        x = self.conv1(x, circular_padding)
+        x = self.drop(x)
+
         x = self.up3(x, x2, circular_padding)
+        x = self.conv0(x, circular_padding)
+        x = self.drop(x)
+
         x = self.up4(x, x1, circular_padding)
+        x = self.conv0(x, circular_padding)
+        x = self.drop(x)
+
         x = self.outc(x)
         return x
--- src/features/utils.py
+++ src/features/utils.py
@@ -3,41 +3,36 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class double_CBR(nn.Module):
-    def __init__(self, in_ch, out_ch, circular_padding, kernel_size):
-        super(double_CBR, self).__init__()
-        if circular_padding :
-            padding_version = (1,0)
-        else : 
-            padding_version = 1
+class CBR(nn.Module):
+    def __init__(self, in_ch, out_ch, circular_padding, kernel_size, padding_size):
+        super(CBR, self).__init__()
+        self.padding_size = padding_size
 
-        self.conv1 = nn.Sequential(
+        if circular_padding :
+            padding_version = (self.padding_size,0)
+        else : 
+            padding_version = self.padding_size
+
+        self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size, padding=padding_version),
-            nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(out_ch, out_ch, kernel_size, padding=padding_version),
             nn.BatchNorm2d(out_ch),
             nn.LeakyReLU(inplace=True)
         )
 
     def forward(self, x, circular_padding):
         if circular_padding : 
-            x = F.pad(x,(1,1,0,0),mode = 'circular')
-            x = self.conv1(x)
-            x = F.pad(x,(1,1,0,0),mode = 'circular')
-            x = self.conv2(x)
+            x = F.pad(x,(self.padding_size,self.padding_size,0,0), mode = 'circular')
+            x = self.conv(x)
         else : 
-            x = self.conv1(x)
-            x = self.conv2(x)
+            x = self.conv(x)
         return x
 
+
 class down_CBR(nn.Module):
-    def __init__(self, in_ch, out_ch, circular_padding, kernel_size):
+    def __init__(self, in_ch, out_ch, circular_padding, kernel_size, padding_size):
         super(down_CBR, self).__init__()
         self.pooling = nn.MaxPool2d(2)
-        self.conv = double_CBR(in_ch, out_ch, circular_padding, kernel_size)
+        self.conv = CBR(in_ch, out_ch, circular_padding, kernel_size, padding_size)
       
     def forward(self, x, circular_padding):
         x = self.pooling(x)
@@ -46,32 +41,36 @@ class down_CBR(nn.Module):
 
 
 class up_CBR(nn.Module):
-    def __init__(self, in_ch, out_ch, circular_padding, kernel_size, drop_p = 0.5, block_size = 7):
+    def __init__(self, in_ch, out_ch, circular_padding, kernel_size, padding_size):
         super(up_CBR, self).__init__()
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv = double_CBR(in_ch, out_ch, circular_padding, kernel_size)
-        self.drop_prob = drop_p
-        self.block_size = block_size
-
+        self.up = self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2, groups = in_ch//2)
+        self.conv = CBR(in_ch, out_ch, circular_padding, kernel_size, padding_size)
+        
     def forward(self, x1, x2, circular_padding):
         x1 = self.up(x1)
         
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-
         x1 = F.pad(x1, (diffX // 2, diffX - diffX//2, diffY // 2, diffY - diffY//2))
+
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x, circular_padding)
+        return x
 
+class BlockDrop(nn.Module):
+    def __init__(self, drop_p = 0.5, block_size = 7):
+        super(BlockDrop, self).__init__()
+        self.drop_prob = drop_p
+        self.block_size = block_size
+
+    def forward(self, x):
         gamma = self.drop_prob / (self.block_size ** 2)
-
         mask = (torch.rand(x.shape[0], *x.shape[2:]) < gamma).float().to(x.device)
 
         block_mask = compute_block_mask(mask, 7)
 
         out = x * block_mask[:, None, :, :]
         out = out * block_mask.numel() / block_mask.sum()
-
         return out
 
 
@@ -91,9 +90,9 @@ def compute_block_mask(mask, block_size):
 
 class BottleNeck(nn.Module):
     expansion = 4
-    def __init__(self, in_channels, out_channels, stride=1):
+    def __init__(self, in_channels, out_channels, circular_padding, stride=1):
         super().__init__()
-
+        self.circular_padding = circular_padding
         self.residual_function = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
             nn.BatchNorm2d(out_channels),
