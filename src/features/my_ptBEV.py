a604096e488da854a926f66f1a7e21d9135cf4ab
zoomin-lee <65997635+zoomin-lee@users.noreply.github.com>
2022-05-29 23:27:47 +0900

    using full batch

--- src/features/my_ptBEV.py
+++ src/features/my_ptBEV.py
@@ -40,17 +40,17 @@ class ptBEVnet(nn.Module):
             nn.BatchNorm1d(fea_dim),
             nn.Linear(fea_dim, 64),
             nn.BatchNorm1d(64),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(64, 128),
             nn.BatchNorm1d(128),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(128, 256),
             nn.BatchNorm1d(256),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(256, out_pt_fea_dim),
         )
 
-        self.fea_compression = nn.Sequential(nn.Linear(out_pt_fea_dim, self.n_height), nn.ReLU())
+        self.make_backbone_input_fea_dim = nn.Sequential(nn.Linear(out_pt_fea_dim, self.n_height), nn.LeakyReLU())
 
         assert backbone in ["UNet", "FCN", "DL"], "backbone name is incorrect"
 
@@ -68,51 +68,46 @@ class ptBEVnet(nn.Module):
         out = []
         for i in range(batch_size):
             
-            batch_pt_fea = pt_fea[i]
-            batch_xy_ind = xy_ind[i]
+            batch_size = len(pt_fea)
+        backbone_input_dim = [batch_size, self.grid_size[0], self.grid_size[1], self.n_height]
+        backbone_data = torch.zeros(backbone_input_dim, dtype=torch.float32).to(device)
+
+        fea, ind = [], []
+        num = 0
+        for i in range(batch_size):
             pt_num = len(pt_fea[i])
+            batch_ind = torch.cat([torch.full((pt_num,1), i).to(device), xy_ind[i]], dim=1)
+            fea.append(pt_fea[i])
+            ind.append(batch_ind)
+            num += pt_num
+        fea, ind = torch.cat(fea, dim=0).to(device), torch.cat(ind, dim=0).to(device)
+            
+        random_ind = torch.randperm(num, device=device)
+        fea, ind = torch.index_select(fea, dim=0, index=random_ind), torch.index_select(ind, dim=0, index=random_ind)
+        unq, unq_inv = torch.unique(ind, return_inverse=True, dim=0)
+        
+        ## Own implementation of torch.unique() function TODO: fix speed problem
+        # x_sort_ind = batch_xy_ind[batch_xy_ind[:,1].sort()[1]]
+        # sort_xy_ind = x_sort_ind[x_sort_ind[:,0].sort()[1]]
+        # unq, unq_inv, unq_cnt = index_sort(sort_xy_ind.detach().cpu().numpy(),
+        #                                    batch_xy_ind.detach().cpu().numpy(),
+        #                                    pt_num)
+        # unq = torch.tensor(unq, dtype=torch.int64, device = device)
+        # unq_inv = torch.tensor(unq_inv, dtype=torch.int64, device = device)
+        # unq_cnt = torch.tensor(unq_cnt, dtype=torch.int64, device = device)
+        
+        pointnet_fea = self.Simplified_PointNet(fea)
+        max_pointnet_fea = torch_scatter.scatter_max(pointnet_fea, unq_inv, dim=0)[0]
 
-            shuffled_ind = torch.randperm(pt_num, device=device)
-            batch_pt_fea = torch.index_select(batch_pt_fea, dim=0, index=shuffled_ind)
-            batch_xy_ind = torch.index_select(batch_xy_ind, dim=0, index=shuffled_ind)
+        backbone_input_fea = self.make_backbone_input_fea_dim(max_pointnet_fea)
+        backbone_data[unq[:,0],unq[:,1],unq[:,2],:] = backbone_input_fea
 
-            unq, unq_inv, unq_cnt = torch.unique(batch_xy_ind, return_inverse=True, return_counts=True, dim=0)
+        to_backbone = backbone_data.permute(0, 3, 1, 2)
+        backbone_fea = self.backbone(to_backbone)        
 
-            ## Own implementation of torch.unique() function TODO: fix speed problem
-            # x_sort_ind = batch_xy_ind[batch_xy_ind[:,1].sort()[1]]
-            # sort_xy_ind = x_sort_ind[x_sort_ind[:,0].sort()[1]]
-            # unq, unq_inv, unq_cnt = index_sort(sort_xy_ind.detach().cpu().numpy(),
-            #                                    batch_xy_ind.detach().cpu().numpy(),
-            #                                    pt_num)
-            # unq = torch.tensor(unq, dtype=torch.int64, device = device)
-            # unq_inv = torch.tensor(unq_inv, dtype=torch.int64, device = device)
-            # unq_cnt = torch.tensor(unq_cnt, dtype=torch.int64, device = device)
+        return backbone_fea
 
-            grp_ind = grp_range_torch(unq_cnt, device)[torch.argsort(torch.argsort(unq_inv))]
-            remain_ind = grp_ind < self.max_pt
-
-            batch_pt_fea = batch_pt_fea[remain_ind, :]
-            batch_xy_ind = batch_xy_ind[remain_ind, :]
-            unq_inv = unq_inv[remain_ind]
-
-            pointnet_fea = self.Simplified_PointNet(batch_pt_fea)
-            max_pointnet_fea = torch_scatter.scatter_max(pointnet_fea, unq_inv, dim=0)[0]
-
-            nheight_pointnet_fea = self.fea_compression(max_pointnet_fea)
-
-            batch_out_data = torch.zeros(batch_out_data_dim, dtype=torch.float32).to(device)
-            batch_out_data[unq[:, 0], unq[:, 1], :] = nheight_pointnet_fea
-
-            out.append(batch_out_data)
-
-        to_cnn = torch.stack(out)
-        to_cnn = to_cnn.permute(0, 3, 1, 2)
-
-        unet_fea = self.backbone(to_cnn)        
-
-        return unet_fea
-
-
+        
 @jit(nopython=True)
 def index_sort(sort_ind, xy_ind, pt_num):
     unq = [sort_ind[0]]
