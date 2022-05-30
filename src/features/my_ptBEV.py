b9d94b14727d8dec46a80d11eff17e24a6db8616
zoomin-lee <65997635+zoomin-lee@users.noreply.github.com>
2022-05-30 23:45:20 +0900

    divided the class

--- src/features/my_ptBEV.py
+++ src/features/my_ptBEV.py
@@ -3,10 +3,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch_scatter
-from numba import jit
 import torchvision.models as models
 
-from src.features.my_BEV_Unet import BEV_Unet
+from src.features.my_BEV_Unet import Unet
 
 class ptBEVnet(nn.Module):
     def __init__(
@@ -18,10 +17,8 @@ class ptBEVnet(nn.Module):
         out_pt_fea_dim=512,
         max_pt_per_encode=256,
         circular_padding=False,
-        device=0
     ):
         super(ptBEVnet, self).__init__()
-
 
         self.max_pt = max_pt_per_encode
         self.backbone_name = backbone_name
@@ -29,7 +26,6 @@ class ptBEVnet(nn.Module):
         self.grid_size = grid_size
         self.n_class = len(n_class)
         self.circular_padding = circular_padding
-        self.device = device
 
         if projection_type == "traditional":
             fea_dim = 7
@@ -38,38 +34,23 @@ class ptBEVnet(nn.Module):
         else:
             AssertionError, "incorrect projection type"
 
-        self.Simplified_PointNet = nn.Sequential(
-            nn.BatchNorm1d(fea_dim),
-            nn.Linear(fea_dim, 64),
-            nn.BatchNorm1d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(64, 128),
-            nn.BatchNorm1d(128),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(128, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(256, out_pt_fea_dim),
-        )
-
-        self.make_backbone_input_fea_dim = nn.Sequential(nn.Linear(out_pt_fea_dim, self.n_height), nn.LeakyReLU())
+        self.PointNet = Simplified_PointNet(fea_dim, out_pt_fea_dim, self.n_height)
 
         assert self.backbone_name in ["UNet", "FCN", "DL"], "backbone name is incorrect"
 
         if self.backbone_name == "UNet":
-            self.backbone = BEV_Unet(self.n_class, self.n_height, self.circular_padding)
+            self.backbone = Unet(self.n_class, self.n_height, self.circular_padding)
             
         elif self.backbone_name == "FCN":
-            self.backbone = models.segmentation.fcn_resnet50(pretrained=False, pretrained_backbone=False, num_classes=19*32).to(self.device)
-            self.backbone.backbone.conv1 = nn.Conv2d(self.n_height, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False).to(self.device)
+            self.backbone = models.segmentation.fcn_resnet50(pretrained=False, pretrained_backbone=False, num_classes=self.n_class*self.n_height)
+            self.backbone.backbone.conv1 = nn.Conv2d(self.n_height, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        
         elif self.backbone_name == "DL":
-            self.backbone = models.segmentation.deeplabv3_resnet101(pretrained=False, pretrained_backbone=False, num_classes=19*32).to(self.device)
-            self.backbone.backbone.conv1 = nn.Conv2d(self.n_height, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False).to(self.device)
+            self.backbone = models.segmentation.deeplabv3_resnet101(pretrained=False, pretrained_backbone=False, num_classes=self.n_class*self.n_height)
+            self.backbone.backbone.conv1 = nn.Conv2d(self.n_height, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
     def forward(self, pt_fea, xy_ind, device):
         batch_size = len(pt_fea)
-        backbone_input_dim = [batch_size, self.grid_size[0], self.grid_size[1], self.n_height]
-        backbone_data = torch.zeros(backbone_input_dim, dtype=torch.float32).to(device)
 
         fea, ind = [], []
         num = 0
@@ -83,66 +64,67 @@ class ptBEVnet(nn.Module):
             
         random_ind = torch.randperm(num, device=device)
         fea, ind = torch.index_select(fea, dim=0, index=random_ind), torch.index_select(ind, dim=0, index=random_ind)
-        unq, unq_inv = torch.unique(ind, return_inverse=True, dim=0)
-        ## Own implementation of torch.unique() function TODO: fix speed problem
-        # x_sort_ind = batch_xy_ind[batch_xy_ind[:,1].sort()[1]]
-        # sort_xy_ind = x_sort_ind[x_sort_ind[:,0].sort()[1]]
-        # unq, unq_inv, unq_cnt = index_sort(sort_xy_ind.detach().cpu().numpy(),
-        #                                    batch_xy_ind.detach().cpu().numpy(),
-        #                                    pt_num)
-        # unq = torch.tensor(unq, dtype=torch.int64, device = device)
-        # unq_inv = torch.tensor(unq_inv, dtype=torch.int64, device = device)
-        # unq_cnt = torch.tensor(unq_cnt, dtype=torch.int64, device = device)
+        unq, unq_inv, unq_cnt = torch.unique(ind, return_inverse=True, return_counts=True, dim=0)
+        
+        grp_ind = grp_range_torch(unq_cnt,device)[torch.argsort(torch.argsort(unq_inv))]
+        remain_ind = grp_ind < self.max_pt
+        fea = fea[remain_ind,:]
+        ind = ind[remain_ind,:]
+        unq_inv = unq_inv[remain_ind]
+        unq_cnt = torch.clamp(unq_cnt,max=self.max_pt)
 
-        pointnet_fea = self.Simplified_PointNet(fea)
-        max_pointnet_fea = torch_scatter.scatter_max(pointnet_fea, unq_inv, dim=0)[0]
+        pointnet_fea = self.PointNet(fea, unq, unq_inv, batch_size, device)
 
-        backbone_input_fea = self.make_backbone_input_fea_dim(max_pointnet_fea)
-        backbone_data[unq[:,0],unq[:,1],unq[:,2],:] = backbone_input_fea
-
-        to_backbone = backbone_data.permute(0, 3, 1, 2)
-        backbone_fea = self.backbone(to_backbone)
-
-        if self.backbone_name != "UNet":
+        if self.backbone_name == "UNet":
+            backbone_fea = self.backbone(pointnet_fea)
+        else:
+            backbone_fea = self.backbone(pointnet_fea)
             backbone_fea = torch.tensor(backbone_fea['out']).to(device)
             class_per_voxel_dim = [batch_size, self.grid_size[0], self.grid_size[1], self.n_height, self.n_class]
             backbone_fea = backbone_fea.permute(0,2,3,1)
             backbone_fea = backbone_fea.view(class_per_voxel_dim).permute(0,4,1,2,3)
-
         return backbone_fea
 
-@jit(nopython=True)
-def index_sort(sort_ind, xy_ind, pt_num):
-    unq = [sort_ind[0]]
-    unq_cnt = []
-    count = 0
-    for ind in sort_ind:
-        if ind[0] == unq[-1][0] and ind[1] == unq[-1][1]:
-            count += 1
-        else:
-            unq.append(ind)
-            unq_cnt.append(count)
-            count = 1
-    unq_cnt.append(count)
 
-    unq_inv = [0 for i in range(pt_num)]
+class Simplified_PointNet(nn.Module):
+    def __init__(self, fea_dim, out_pt_fea_dim, n_height, h, w):
+        self.h, self.w, self.c = h, w, n_height
+        
+        self.PointNet = nn.Sequential(
+            nn.BatchNorm1d(fea_dim),
+            nn.Linear(fea_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(256, out_pt_fea_dim),
+        )
 
-    for i, ind in enumerate(unq):
-        boolen = np.where(xy_ind == ind, True, False)
-        condition = []
-        for j, boo in enumerate(boolen):
-            tf = np.all(boo)
-            if tf:
-                condition.append(j)
-        for cond in condition:
-            unq_inv[cond] = i
-    return unq, unq_inv, unq_cnt
+        self.make_backbone_input_fea_dim = nn.Sequential(
+            nn.Linear(out_pt_fea_dim, self.c), 
+            nn.LeakyReLU()
+        )
 
+    def forward(self, fea, unq, unq_inv, n, device):
+        backbone_input_dim = [n, self.h, self.w, self.c]
+        backbone_data = torch.zeros(backbone_input_dim, dtype=torch.float32).to(device)
+        pointnet_fea = self.PointNet(fea)
+        max_pointnet_fea = torch_scatter.scatter_max(pointnet_fea, unq_inv, dim=0)[0]
+        #max_pointnet_fea = []
+        #for i in range(len(unq)):
+        #    max_pointnet_fea.append(torch.max(pointnet_fea[unq_inv==i],dim=0)[0])
+        #max_pointnet_fea = torch.stack(max_pointnet_fea)
+        backbone_input_fea = self.make_backbone_input_fea_dim(max_pointnet_fea)
+        backbone_data[unq[:,0],unq[:,1],unq[:,2],:] = backbone_input_fea
+        return backbone_data.permute(0, 3, 1, 2)
 
-# TODO: cite or remove
-def grp_range_torch(a, dev):
-    idx = torch.cumsum(a, 0)
-    id_arr = torch.ones(idx[-1], dtype=torch.int64, device=dev)
+def grp_range_torch(a,dev):
+    idx = torch.cumsum(a,0)
+    id_arr = torch.ones(idx[-1],dtype = torch.int64,device=dev)
     id_arr[0] = 0
-    id_arr[idx[:-1]] = -a[:-1] + 1
-    return torch.cumsum(id_arr, 0)
+    id_arr[idx[:-1]] = -a[:-1]+1
+    return torch.cumsum(id_arr,0)
