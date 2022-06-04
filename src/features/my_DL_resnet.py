0b92721a64f0d98fd8b400b9ec1018361c0df1bc
zoomin-lee <65997635+zoomin-lee@users.noreply.github.com>
2022-06-04 11:12:22 +0900

    Update my_DL_resnet.py

--- src/features/my_DL_resnet.py
+++ src/features/my_DL_resnet.py
@@ -1,10 +1,9 @@
-from tkinter import X
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from src.features.layer_functions import Res_block, up_CBR, CBR
+from src.features.layer_functions import Res_block, reshape_to_voxel
 
 class ResNet_DL(nn.Module):
     def __init__(self, n_class, n_height, circular_padding, grid_size):
@@ -13,7 +12,11 @@ class ResNet_DL(nn.Module):
         self.n_class= n_class
         self.n_height = n_height
 
-        self.conv1 = CBR(in_ch=n_height, out_ch=64, circular_padding=False, filter_size=7, stride=2, padding_size=3)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(n_height, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True)
+        )
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
         conv2_in_ch, conv2_mid_ch, conv2_out_ch = [64, 256, 256], [64 for i in range(3)], [256 for i in range(3)]
@@ -31,9 +34,15 @@ class ResNet_DL(nn.Module):
         conv5_in_ch, conv5_mid_ch, conv5_out_ch = [1024,2048,2048], [512 for i in range(3)], [2048 for i in range(3)]
         layers5 = [Res_block(i, m , o, self.circular_padding) for i, m , o in zip(conv5_in_ch, conv5_mid_ch, conv5_out_ch)]
         self.conv5_x = nn.Sequential(*layers5)
-        self.ASPP_module = ASPP(2048, 32, self.n_class*self.n_height)
-        self.up = nn.Upsample(size=(grid_size[0], grid_size[1]), mode= 'bilinear')
-
+        self.ASPP_module = ASPP(2048, 32, 256)
+        self.up1 = nn.Upsample(size=(grid_size[0]//4, grid_size[1]//4), mode= 'bilinear')
+        self.concat = nn.Conv2d(512, 48, kernel_size=1)
+        self.classifier = nn.Sequential(
+            nn.Conv2d(304, 64, kernel_size=3),
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.Conv2d(64, self.n_class*self.n_height, kernel_size=1)
+        )
+        self.up2 = nn.Upsample(size=(grid_size[0], grid_size[1]), mode= 'bilinear')
 
     def forward(self,x):                        # 2, 32, 480, 360
         x1 = self.conv1(x)                      # 2, 64, 240, 180
@@ -41,17 +50,15 @@ class ResNet_DL(nn.Module):
         x3 = self.conv3_x(x2)                   # 2, 512, 120, 90
         x4 = self.conv4_x(x3)                   # 2, 1024, 60,45
         x5 = self.conv5_x(x4)                   # 2, 2048, 30,23
-        x = self.ASPP_module(x5)                # 2, 608, 30, 23
-        x = self.up(x)                          # 2, 608, 480, 360
-        
-        x = x.permute(0, 2, 3, 1)
-        class_per_voxel_dim = [x.size()[0], x.size()[1], x.size()[2], self.n_height, self.n_class]
-        x = x.view(class_per_voxel_dim).permute(0, 4, 1, 2, 3)
-
+        out1 = self.up1(self.ASPP_module(x5))    # 2, 256, 120, 90
+        out2 = self.concat(x3)                    # 2, 48, 120, 90
+        x = torch.cat([out1, out2], 1)          # 2, 304, 120, 90
+        x = self.up2(self.classifier(x))
+        x = reshape_to_voxel(x, self.n_height, self.n_class)
         return x
 
 class ASPP(nn.Module):
-    def __init__(self, in_channels, out_channels, num_classes):
+    def __init__(self, in_channels, out_channels, concat_channels):
         super(ASPP, self).__init__()
         
         self.ASPP1 = nn.Sequential(
@@ -85,7 +92,7 @@ class ASPP(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
-        self.classifier = nn.Conv2d(out_channels, num_classes, kernel_size=1)
+        self.conv2 = nn.Conv2d(out_channels, concat_channels, kernel_size=1)
 
     def forward(self, x):
         x_h, x_w = x.size()[2], x.size()[3]
@@ -95,6 +102,6 @@ class ASPP(nn.Module):
 
         out = torch.cat([out1, out2, out3, out4, out5], 1) 
         out = self.conv(out)
-        out = self.classifier(out) 
+        out = self.conv2(out) 
 
         return out
